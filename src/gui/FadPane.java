52ace7609cda4bb706ec02210dbdcb12316279a3
JohnLe <johnle@hotmail.dk>
2023-03-24 19:07:36 +0100

    OpretfadWindow

--- src/gui/FadPane.java
+++ src/gui/FadPane.java
@@ -10,10 +10,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import logik.Fad;
-import logik.Lager;
-import logik.Reol;
 
 public class FadPane extends GridPane {
 
@@ -21,11 +19,17 @@ public class FadPane extends GridPane {
     private Button  btnOpretFad;
     private Button btnFjernFad;
 
+    private HBox btnFadBox, btnServBox;
+
+    private OpretFadWindow opretFadWindow;
+
     public FadPane() {
         this.setGridLinesVisible(false);
         this.setPadding(new Insets(20));
         this.setHgap(20);
         this.setVgap(10);
+
+        opretFadWindow = new OpretFadWindow("Opret fad",new Stage());
 
         lvwFade = new ListView<>();
         this.add(lvwFade, 1, 1, 1, 1);
@@ -38,25 +42,36 @@ public class FadPane extends GridPane {
 
         Label lblFade = new Label("Fade");
         this.add(lblFade, 0, 0);
+        lblFade.setAlignment(Pos.TOP_LEFT);
+
+        btnFadBox = new HBox();
+        this.add(btnFadBox, 0, 4);
+        btnFadBox.setSpacing(20);
+
 
         btnOpretFad = new Button("Opret");
+        btnFadBox.getChildren().add(btnOpretFad);
         btnOpretFad.setOnAction(event -> this.opretFadAction());
-        btnFjernFad = new Button("Fjern");
-        btnFjernFad.setOnAction(event -> this.removeFadAction());
-        VBox fadButtons = new VBox(10, btnOpretFad, btnFjernFad);
-        fadButtons.setAlignment(Pos.CENTER);
 
+        btnFjernFad = new Button("Fjern");
+        btnFadBox.getChildren().add(btnFjernFad);
+        btnFjernFad.setOnAction(event -> this.removeFadAction());
+
+
+        /*
+        VBox fadButtons = new VBox(10, btnOpretFad, btnFjernFad);
+        fadButtons.setAlignment(Pos.BOTTOM_LEFT);
 
         HBox lagerHBox = new HBox(lblFade, lvwFade, fadButtons);
-
         lagerHBox.setSpacing(10);
+        this.add(lagerHBox, 0, 3);
 
-        this.add(lagerHBox, 0, 0);
+         */
 
     }
 
     private void opretFadAction() {
-        Controller.opretLager();
+        opretFadWindow.showAndWait();
         lvwFade.getItems().setAll(Controller.getFadMap().values());
     }
     private void removeFadAction() {
