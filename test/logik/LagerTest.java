4b24e12ccf7595eeb7ca0ff23fd66c65626cf26c
JohnLe <johnle@hotmail.dk>
2023-03-27 00:36:54 +0200

    Test

--- test/logik/LagerTest.java
+++ test/logik/LagerTest.java
@@ -3,31 +3,50 @@ package logik;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LagerTest {
-
+private Lager lager;
+private Reol reol;
     @org.junit.jupiter.api.Test
     void createReol() {
         //Arrange
+        lager = new Lager();
 
         //Act
+        Reol reol = lager.createReol();
 
         //Assert
+        assertNotNull(reol);
+        assertEquals(1, lager.getReolMap().size()); // Tjek om der er tilføjet en ny reol til lager
+        assertTrue(lager.getReolMap().containsKey(reol.getReolId())); // Tjek om reol findes i lagers hyldeMap
     }
 
     @org.junit.jupiter.api.Test
     void addReol() {
         //Arrange
+        lager = new Lager();
+        reol = new Reol();
 
         //Act
+        lager.addReol(reol);
 
         //Assert
+        lager.getReolMap().containsValue(reol);
     }
 
     @org.junit.jupiter.api.Test
     void removeReol() {
         //Arrange
+        Lager lager = new Lager();
+        Reol reol = lager.createReol();
+        Hylde hylde1 = reol.createHylde();
+        Hylde hylde2 = reol.createHylde();
 
         //Act
+        lager.removeReol(reol.getReolId());
 
         //Assert
+        assertNull(lager.getReol(reol.getReolId())); // Tjek om reolen er blevet fjernet fra lageret
+        assertEquals(0, lager.getReolMap().size()); // Tjek om lageret er tomt
+        assertNull(reol.getHylde(hylde1.getHyldeId())); // Tjek om den første hylde er blevet fjernet fra reolen
+        assertNull(reol.getHylde(hylde2.getHyldeId())); // Tjek om den anden hylde er blevet fjernet fra reolen
     }
 }
--- test/logik/ReolTest.java
+++ test/logik/ReolTest.java
@@ -5,22 +5,34 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReolTest {
+    private Reol reol;
+    private Hylde hylde;
 
     @Test
     void createHylde() {
         //Arrange
+        Reol reol = new Reol();
 
         //Act
+        Hylde hylde = reol.createHylde();
 
         //Assert
+        assertNotNull(hylde);
+        assertEquals(1, reol.getHyldeMap().size()); // Tjek om der er tilføjet en ny hylde til reolen
+        assertTrue(reol.getHyldeMap().containsKey(hylde.getHyldeId())); // Tjek om hylde findes i reolens hyldeMap
     }
+
 
     @Test
     void addHylde() {
         //Arrange
+        reol = new Reol();
+        hylde = new Hylde();
 
         //Act
+        reol.addHylde(hylde);
 
         //Assert
+        assertTrue(reol.getHyldeMap().containsValue(hylde));
     }
 }
