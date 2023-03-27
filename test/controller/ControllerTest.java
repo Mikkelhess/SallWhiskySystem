02068597982a6d15365c1e91ff9c2430386b1d34
JohnLe <johnle@hotmail.dk>
2023-03-27 17:23:31 +0200

    Test

--- test/controller/ControllerTest.java
+++ test/controller/ControllerTest.java
@@ -1,12 +1,29 @@
 package controller;
 
+import logik.Fad;
+import logik.FadType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ControllerTest {
-
+    Fad fad;
+    @BeforeEach
+    void setUp(){
+        this.fad = new Fad(20.00, FadType.BOURBON);
+    }
     @Test
     void opretFad() {
+            //Arrange
+            Controller controller = Controller.getController();
+
+            //Act
+            double faktiskResultat = controller.
+
+            //Assert
+            double forventetResultat = 0;
+            assertEquals(forventetResultat,faktiskResultat);
+        }
     }
 }
