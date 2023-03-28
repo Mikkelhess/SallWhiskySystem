3ab01e36bdc53f8757f5ef2fbdf21ed84a4cf19a
Mikkel <mikkelhess@icloud.com>
2023-03-28 09:36:43 +0200

    nye klasser

--- src/gui/StartWindow.java
+++ src/gui/StartWindow.java
@@ -1,59 +1,62 @@
-package gui;
+    package gui;
 
-import controller.Controller;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
+    import controller.Controller;
+    import javafx.application.Application;
+    import javafx.scene.Scene;
+    import javafx.scene.control.Tab;
+    import javafx.scene.control.TabPane;
+    import javafx.scene.layout.Border;
+    import javafx.scene.layout.BorderPane;
+    import javafx.scene.layout.GridPane;
+    import javafx.stage.Stage;
 
-public class StartWindow extends Application {
+    public class StartWindow extends Application {
 
-    @Override
-    public void init() {
-        Controller.initStorage();
+
+        //TODO Inkorporer en comparator/compareTo der sorterer listviews på id'et
+
+        @Override
+        public void init() {
+            Controller.initStorage();
+        }
+
+        @Override
+        public void start(Stage stage) {
+            stage.setTitle("Sall Whisky System");
+            BorderPane pane = new BorderPane();
+            this.initContent(pane);
+
+            Scene scene = new Scene(pane);
+            stage.setScene(scene);
+            stage.show();
+        }
+
+        //--------------------------------------------------------------------
+
+        private void initContent(BorderPane pane) {
+            TabPane tabPane = new TabPane();
+            this.initTabPane(tabPane);
+            pane.setCenter(tabPane);
+        }
+
+        private void initTabPane(TabPane tabPane) {
+            tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
+
+            Tab tabLagerManager = new Tab("Lager Manager");
+            tabPane.getTabs().add(tabLagerManager);
+
+            LagerPane lagerPane = new LagerPane();
+            tabLagerManager.setContent(lagerPane);
+            tabLagerManager.setOnSelectionChanged(event -> lagerPane.updateList());
+
+            Tab tabKonference = new Tab("Fad");
+            tabPane.getTabs().add(tabKonference);
+
+            FadPane fadPane = new FadPane();
+            tabKonference.setContent(fadPane);
+            tabKonference.setOnSelectionChanged(event -> fadPane.updateList());
+
+        }
+
+
     }
-
-    @Override
-    public void start(Stage stage) {
-        stage.setTitle("Sall Whisky System");
-        BorderPane pane = new BorderPane();
-        this.initContent(pane);
-
-        Scene scene = new Scene(pane);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    //--------------------------------------------------------------------
-
-    private void initContent(BorderPane pane) {
-        TabPane tabPane = new TabPane();
-        this.initTabPane(tabPane);
-        pane.setCenter(tabPane);
-    }
-
-    private void initTabPane(TabPane tabPane) {
-        tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
-
-        Tab tabLagerManager = new Tab("Lager Manager");
-        tabPane.getTabs().add(tabLagerManager);
-
-        LagerPane lagerPane = new LagerPane();
-        tabLagerManager.setContent(lagerPane);
-        tabLagerManager.setOnSelectionChanged(event -> lagerPane.updateList());
-
-        Tab tabKonference = new Tab("Fad");
-        tabPane.getTabs().add(tabKonference);
-
-        FadPane fadPane = new FadPane();
-        tabKonference.setContent(fadPane);
-        tabKonference.setOnSelectionChanged(event -> fadPane.updateList());
-
-    }
-
-
-}
---
+++ src/logik/Whisky.java
@@ -0,0 +1,17 @@
+package logik;
+
+import java.util.ArrayList;
+
+public class Whisky {
+
+    private String kornBeskrivelse;
+    private String destillatAnsvarlig;
+    private String destilleringsproces;
+    //hvordan destilleringen er foregået (hos Sall destilleres altid 2 gange)
+    private ArrayList<Fad> fadListe;
+    private WhiskyHistorie whiskyHistorie;
+
+
+
+
+}
