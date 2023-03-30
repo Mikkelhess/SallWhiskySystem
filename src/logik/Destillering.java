2fc4977d213275fee980dc3992533c9cee232016
jjohnsen123desktop <johnsenjeppe@gmail.com>
2023-03-30 14:02:31 +0200

    checkIfTilgængeligLiter metode tilføjet

--- src/logik/Destillering.java
+++ src/logik/Destillering.java
@@ -1,7 +1,10 @@
 package logik;
 
+import com.sun.jdi.Value;
+
 import java.time.LocalDate;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Destillering {
 
@@ -33,6 +36,24 @@ public class Destillering {
         addDestillat(destillat.getNewMakeNummer(), destillat);
         return destillat;
     }
+
+    /**
+     * Looper igennem destillatMap vha. for-each loop
+     * og checker om hvorvidt liter er mindre eller lig med litreBrugt - totalLiter
+     * @param liter den værdi som skal tjekkes imod litre som er tilgængelige
+     * @return true eller false alt efter om der er litre tilgængelig
+     */
+    public boolean checkIfTilgængeligLiter(double liter) {
+        double litreBrugt = 0;
+        for (Map.Entry<Integer, Destillat> entry : destillatMap.entrySet()) {
+            litreBrugt += entry.getValue().getLiter();
+        }
+        if (liter <= (totalLiter - litreBrugt)) {
+            return true;
+        }
+        return false;
+    }
+
 
     public void addDestillat(int newMakeNummer, Destillat destillat) {
         destillatMap.put(newMakeNummer, destillat);
